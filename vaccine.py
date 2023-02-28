c0d63d12ac85338b299ff44ba5e0f899e5100fa1
andrewypowers <andypowers1@gmail.com>
2023-02-28 16:15:26 -0500

    calculate proportions

--- vaccine.py
+++ vaccine.py
@@ -43,12 +43,17 @@ ae_freq = alt.Chart(subset).mark_bar().encode(
 st.altair_chart(ae_freq, use_container_width = False)
 
 #adverse event proportion chart
-ae_prop = alt.Chart(subset).mark_bar().encode(
-    alt.X('vaccine:N', axis = alt.Axis(title = None, labels = False)),
-    alt.Y('count:Q', scale = alt.Scale(type = 'linear'), axis = alt.Axis(grid = False), title = 'Proportion'),
-    alt.Column('event_type:N', header = alt.Header(titleOrient = 'bottom', labelOrient = 'bottom', labelColor = 'white'), title = ''),
-    alt.Color('vaccine:N')
-    ).properties(title = 'Adverse event proportion, filtered by vaccine and seriousness, stratified by event type'
-    ).configure_title(color = 'white')
+ae_prop = alt.Chart(subset).mark_bar(
+    ).transform_joinaggregate(
+        total_event = 'sum(count)'
+    ).transform_calculate(
+        event_percent = ' datum.count / datum.total_event'
+    ).encode(
+        alt.X('vaccine:N', axis = alt.Axis(title = None, labels = False)),
+        alt.Y('event_percent:Q', scale = alt.Scale(type = 'linear'), axis = alt.Axis(grid = False), title = 'Proportion'),
+        alt.Column('event_type:N', header = alt.Header(titleOrient = 'bottom', labelOrient = 'bottom', labelColor = 'white'), title = ''),
+        alt.Color('vaccine:N')
+        ).properties(title = 'Adverse event proportion, filtered by vaccine and seriousness, stratified by event type'
+        ).configure_title(color = 'white')
 
 st.altair_chart(ae_prop, use_container_width = False)
